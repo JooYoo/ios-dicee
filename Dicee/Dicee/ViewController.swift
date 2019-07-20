f0b9706e9935822c11f250c534a4d236923fc8ef
JooYoo <jooyoo@outlook.com>
2019-07-20 07:51:55 +0200

    created project

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by JooYoo on 2019/7/20.
+//  Copyright © 2019 JooYoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
